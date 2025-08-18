277653630fd5fcce91ce619423529c690e616c66
Fanny58 <famm4youu@gmail.com>
2025-08-18 16:12:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,9 @@
-// File: config.js
-
-// Import fungsi yang kita butuhkan dari Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-
-// =======================================================
-// === KODE KONFIGURASI "ARTAKITA" ANDA ===
-// =======================================================
-const firebaseConfig = {
-  apiKey: "AIzaSyBP7DUADOb1yh6sj0XMvN3OqhEtAE4tfg0",
-  authDomain: "artakita-ca7fd.firebaseapp.com",
-  projectId: "artakita-ca7fd",
-  storageBucket: "artakita-ca7fd.appspot.com",
-  messagingSenderId: "733822646721",
-  appId: "1:733822646721:web:cfb9f73afb0739425a9740",
+const CONFIG = {
+  // GANTI DENGAN URL WEB APP BARU DARI LANGKAH 2
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbyVAvkeTyBn5_yYGlLl04nrf8fajsVTaemdwVz2AMJI3A4hSDvC9kske_pbZ_wt3qiBtA/exec",
+  USER: {
+    username: "admin",
+    password: "123",
+  },
 };
-// =======================================================
-
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Konfigurasi login admin (tetap sama)
-const ADMIN_USER = {
-  username: "admin",
-  password: "12345",
-};
-
-// Ekspor variabel db dan ADMIN_USER agar bisa dipakai di script.js
-export { db, ADMIN_USER };
